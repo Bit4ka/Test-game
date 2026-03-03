26d0c5be957d59a7f760a5d93e0675d211f64686
Bit4ka <denisderex@gmail.com>
2026-03-03 19:53:00 +0100

    Map
    mapa i drzwi

---
+++ Test Game/Assets/Scripts/Map/Door.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public enum Direction { North, East, South, West }  // Kierunek drzwi
+    public Direction doorDirection;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {  // Taguj gracza "Player"
+            // Tutaj logika zmiany pokoju, np. RoomManager.Instance.MoveToNextRoom(doorDirection);
+            Debug.Log("Wejście przez drzwi: " + doorDirection);
+        }
+    }
+}
