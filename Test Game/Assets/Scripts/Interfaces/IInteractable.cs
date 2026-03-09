3f1b5127685ab8d6208945e257b14b019b529f4f
Waldek4500 <zygmuntowiczwaldek@gmail.com>
2026-03-09 20:44:36 +0100

    Zmienianie pmiesczeń
    Dodałem system interakcji.
    Przy drzwiach przytrzymujesz E i przenosi cię do innego pokoju. Zrobione w scenie Level1.
    W kontenerze MapBoundry są PolygonCollidery, każdy dla osobnego pokoju.
    Trzeba ustawić konkretny dla pojedynczych drzwi (skrypt Door) oraz dodać pusty gameObject (np. jako dziecko drzwi), do którego zostanie przeniesiony gracz po interakcji.
    Ikonka wykrzyknika oznacza, że można z czymś wejść w interakcję.
    I po co ja tyle piszę i tak nikt tego nie przeczyta :)

---
+++ Test Game/Assets/Scripts/Interfaces/IInteractable.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public interface IInteractable
+{
+    public bool CanInteract(GameObject interactor);
+
+    public void Interact(GameObject interactor);
+}
--- Test Game/Assets/Scripts/Map/Door.cs
+++ Test Game/Assets/Scripts/Map/Door.cs
@@ -1,15 +1,22 @@
-﻿using UnityEngine;
+﻿using Unity.Cinemachine;
+using UnityEngine;
 
-public class Door : MonoBehaviour
+public class Door : MonoBehaviour, IInteractable
 {
     public enum Direction { North, East, South, West }
     public Direction doorDirection;
 
     private Animator animator;
 
+    [SerializeField] private float _interactionRange = 2;
+    [SerializeField] private Transform _placeToGo;
+    [SerializeField] PolygonCollider2D _mapBoundry;
+    private CinemachineConfiner2D _confiner;
+
     void Start()
     {
         animator = GetComponent<Animator>();
+        _confiner = FindAnyObjectByType<CinemachineConfiner2D>();
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -29,5 +36,30 @@ public class Door : MonoBehaviour
             animator.SetTrigger("Close");
             Debug.Log("Zamykam drzwi: " + doorDirection);
         }
+    }
+
+    public void Interact(GameObject interactor)
+    {
+        if (_placeToGo == null)
+        {
+            Debug.LogWarning("Place to go not set");
+            return;
+        }
+
+        if (_mapBoundry == null)
+        {
+            Debug.LogWarning("Map boundry not set");
+            return;
+        }
+
+        _confiner.BoundingShape2D = _mapBoundry;
+        interactor.transform.position = _placeToGo.position;
+        
+    }
+
+    public bool CanInteract(GameObject interactor)
+    {
+        float distance = Vector2.Distance(interactor.transform.position, transform.position);
+        return distance < _interactionRange;
     }
 }
